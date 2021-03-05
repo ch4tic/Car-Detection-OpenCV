26eefa31f8a1b41d48a82603f3994944c5960ec3
ch4tic <belusic012@gmail.com>
2021-03-05 22:33:42 +0100

    everything is done

--- src/main.py
+++ src/main.py
@@ -3,30 +3,37 @@
 import cv2
 import os 
 
-car_model = cv2.CascadeClassifier('../data/haarcascade_car.xml')
+car_model = cv2.CascadeClassifier('../data/haarcascade_car.xml') # loading the car detection model
 
-# function for recieving image frames and drawing rectangles around it using the detected coordinates
+# function for finding the correct coordinates of cars and drawing rectangles around them
 def car_detection(frame):
-    cars = car_model.detectMultiScale(frame, 1.15, 4)
+    # detecting cars and drawing blue rectangles to mark them
+    cars = car_model.detectMultiScale(frame, 1.15, 4) 
     for(x, y, w, h) in cars:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), color=(255, 0, 0), thickness=2)
+        color_text = (255, 20, 99)
+        color_rectangle = (255, 20, 99)
+        fontScale = 1
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        image = cv2.rectangle(frame, (x,y), (x+w, y+h), color_rectangle, thickness=2)
+        cv2.putText(image, 'Car', (x, y-10), font, 0.5, color_text, 2)
     return frame
 
 # function for loading and rendering the given video
-def simulator():
-    video = cv2.VideoCapture('../data/cars.mp4')
+def main():
+    video = cv2.VideoCapture('../data/cars2.mp4') 
     while video.isOpened():
         ret, frame = video.read()
-        controlkey = cv2.waitKey(1)
+        exitKey = cv2.waitKey(1)
         if ret:
-            cars_frame = car_detection(frame)
-            cv2.imshow('frame', cars_frame)
+            cars_frame = car_detection(frame) # calling the car_detection function and drawing rectangles
+            cv2.imshow('frame', cars_frame) # showing the video 
         else:
             break
-        if controlkey == ord('q'):
+        # if user presses 'q' the program ends
+        if exitKey == ord('q'):
             break
     video.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    simulator()
+    main()
